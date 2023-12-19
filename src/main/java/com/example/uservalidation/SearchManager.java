6ed1776de7a91d852a876200df5b6d643cd34314
omarWael000 <owael4368@gmail.com>
2023-12-19 19:32:17 +0200

    omar wael's classes

--- src/main/java/com/example/uservalidation/SearchManager.java
+++ src/main/java/com/example/uservalidation/SearchManager.java
@@ -38,6 +38,39 @@ public class SearchManager {
     }
 
 
+    public ArrayList<Movie> searchGenra(String searchedItem)
+    {
+
+        ArrayList<Movie>movie=null;
+        if(searchedItem.equals("Comedy")||searchedItem.equals("comedy"))
+            movie=Genra.getComedyMovie();
+
+       else if(searchedItem.equals("Drama")||searchedItem.equals("drama"))
+            movie=Genra.getDramaMovie();
+
+       else if(searchedItem.equals("Romanc")||searchedItem.equals("romanc"))
+            movie=Genra.getRomancMovie();
+
+        else if(searchedItem.equals("Action")||searchedItem.equals("action"))
+            movie=Genra.getActionMovie();
+
+        else if(searchedItem.equals("Horror")||searchedItem.equals("horror"))
+            movie=Genra.getHorrorMovie();
+
+        else if(searchedItem.equals("For kids")||searchedItem.equals("for kids"))
+            movie=Genra.getForkidsMovie();
+
+        else if(searchedItem.equals("International")||searchedItem.equals("international"))
+            movie=Genra.getInternationalMovie();
+
+        else if(searchedItem.equals("Social")||searchedItem.equals("social"))
+            movie=Genra.getSocialMovie();
+
+
+        return movie;
+
+
+    }
 
 
 }
